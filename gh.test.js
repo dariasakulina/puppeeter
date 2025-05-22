ca9f2d4560fe023b2352ad6661c53806b8ab4d44
Daria Sakulina <evon620c@bk.ru>
2025-05-22 23:53:17 +0300

    first done

--- gh.test.js
+++ gh.test.js
@@ -1,42 +1,34 @@
 let page;
-jest.setTimeout(60000); 
 
 beforeEach(async () => {
   page = await browser.newPage();
-});
+  await page.goto("https://github.com/team");
+}, 60000);
 
 afterEach(() => {
   page.close();
-});
+}, 60000);
 
 describe("Github page tests", () => {
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
+  }, 60000);
 
-  beforeEach(async () => {
-    await page.goto("https://github.com/team");
-  });
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 60000);
 
-    test("The h1 header content'", async () => {
-      jest.setTimeout(80000);
-      const firstLink = await page.$("header div div a");
-      await firstLink.click();
-      await page.waitForSelector("h1");
-      const title2 = await page.title();
-      expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub");
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
     });
-
-    test("The first link attribute", async () => {
-      jest.setTimeout(10000);
-      const actual = await page.$eval("a", (link) => link.getAttribute("href"));
-      expect(actual).toEqual("#start-of-content");
-    });
-
-    test("The page contains Sign in button", async () => {
-      jest.setTimeout(10000);
-      const btnSelector = " a[class='btn-mktg btn-large-mktg btn-muted-mktg']";
-      await page.waitForSelector(btnSelector, {
-       visible: true,
-      });
-      const actual = await page.$eval(btnSelector, (link) => link.textContent);
-      expect(actual).toContain("Sign up for free");
-    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  }, 60000);
 });
